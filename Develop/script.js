f3ed4df08a66a9d9ddb062bdfb4017ec332107ed
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-05-16 15:26:12 -0400

    added prompts and ability to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,12 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var length = alert("Your password WILL BE 12 characters long");
+var uppercase = window.confirm("Do you want Uppercase characters?");
+var lowercase = window.confirm("Do you want Lowercase characters?");
+var _symbols = window.confirm("Do you want symbols?");
+var _numbers = window.confirm("Do you want numbers?");
 
 // Write password to the #password input
 function writePassword() {
@@ -10,8 +14,20 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+//set functions to generate random characters
+function generatePassword() {
+  var length = 12;
+  var charset =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+?",
+    pass = "";
+  for (var i = 0, n = charset.length; i < length; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+  return pass;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+generatePassword();
