e1c0a22c6b3e8e9642035d19481a8176a67d6745
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-05-16 16:02:15 -0400

    add ability to pick the length of the password

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,94 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-var length = alert("Your password WILL BE 12 characters long");
-var uppercase = window.confirm("Do you want Uppercase characters?");
-var lowercase = window.confirm("Do you want Lowercase characters?");
-var _symbols = window.confirm("Do you want symbols?");
-var _numbers = window.confirm("Do you want numbers?");
+
+var lowerChar = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperChar = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numberchar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var symbolChar = [
+  "!",
+  "%",
+  "&",
+  ",",
+  "*",
+  "+",
+  "-",
+  ".",
+  "/",
+  "<",
+  ">",
+  "?",
+  "~",
+];
+
+var confirmUppercase = window.confirm("Do you want Uppercase characters?");
+var confirmLowercase = window.confirm("Do you want Lowercase characters?");
+var confirmSymbols = window.confirm("Do you want symbols?");
+var confirmNumbers = window.confirm("Do you want numbers?");
+//var link = document.getElementById(generate)
+
+var confirmLength = prompt("How long would you like your password to be?");
+if (confirmLength <= 8 || confirmLength >= 128) {
+  alert("Your password has to be between 8-128 characters");
+  //var confirmLength = prompt("How long would you like your password to be?");
+} else {
+  alert("Your password will have " + confirmLength + " characters");
+  console.log(confirmLength);
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -17,17 +100,21 @@ function writePassword() {
 }
 
 //set functions to generate random characters
-function generatePassword() {
-  var length = 12;
-  var charset =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+?",
-    pass = "";
-  for (var i = 0, n = charset.length; i < length; ++i) {
-    pass += charset.charAt(Math.floor(Math.random() * n));
-  }
-  return pass;
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generatePassword();
+
+function getLower() {
+  //0-26
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getUpper() {
+  //27-54
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getSymbols() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+function getNumbers() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
